1052546a395b672610f97b2ba0264fc91a861349
adamtj <atjulier@gmail.com>
2019-07-22 22:10:08 -0400

    clone styles to new element

--- cloner.js
+++ cloner.js
@@ -1,8 +1,15 @@
 window.onload = function(){
+    let styleString = '';
+
     let para = document.querySelector('.content');
     let compStyles = window.getComputedStyle(para);
+
     for(let style in compStyles) {
-      console.log(style, compStyles[style])
+      // console.log(style, compStyles[style]);
+      styleString += `${style}:${compStyles[style]};`;
     }
-    console.log('font size: ', compStyles['font-size'])
+    // console.log('font size: ', compStyles['font-size'])
+
+    document.querySelector('.new').setAttribute('style', styleString)
+
 }
